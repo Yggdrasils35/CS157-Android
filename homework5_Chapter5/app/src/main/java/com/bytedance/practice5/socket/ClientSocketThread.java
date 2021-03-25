a284700adb1f66d012e9f3e77d4cca4723224609
Yggdrasils35 <1026546298@qq.com>
2021-03-26 01:17:10 +0800

    Homework 5 finished.

--- homework5_Chapter5/app/src/main/java/com/bytedance/practice5/socket/ClientSocketThread.java
+++ homework5_Chapter5/app/src/main/java/com/bytedance/practice5/socket/ClientSocketThread.java
@@ -14,20 +14,39 @@ public class ClientSocketThread extends Thread {
     }
 
     private SocketActivity.SocketCallback callback;
+    private boolean stopFlag = false;
+    private volatile String message = "";
 
     //head请求内容
     private static String content = "HEAD /xxjj/index.html HTTP/1.1\r\nHost:www.sjtu.edu.cn\r\n\r\n";
 
+    private synchronized void clearMsg() {
+        this.message = "";
+    }
 
     @Override
     public void run() {
-        // TODO 6 用socket实现简单的HEAD请求（发送content）
-        //  将返回结果用callback.onresponse(result)进行展示
         try {
-            Socket socket = new Socket("localhost", 30000);
+            Socket socket = new Socket("www.sjtu.edu.cn", 80);
             BufferedOutputStream os = new BufferedOutputStream(socket.getOutputStream());
             BufferedInputStream is = new BufferedInputStream(socket.getInputStream());
-
+            message = content;
+            byte[] data = new byte[1024*5];
+            while (!stopFlag && socket.isConnected()) {
+                if (!message.isEmpty()) {
+                    Log.d("socket", "客户端发送"+message);
+                    os.write(message.getBytes());
+                    os.flush();
+                    clearMsg();
+                    int recivelen = is.read(data);
+                    String receive = new String(data, 0, recivelen);
+                    callback.onResponse(receive);
+                }
+            }
+            Log.e("socket", "客户端断开");
+            os.flush();
+            os.close();
+            socket.close();
 
         } catch (Exception e) {
             e.printStackTrace();
